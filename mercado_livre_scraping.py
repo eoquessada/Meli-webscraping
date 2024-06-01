defcd89fe77c204f0d8cd253f981c748f9524d5b
eoquessada <eoquessada@gmail.com>
2024-06-01 07:34:26 -0300

    Initial Commit

---
+++ mercado_livre_scraping.py
@@ -0,0 +1,49 @@
+from bs4 import BeautifulSoup
+import requests
+
+def get_response_text(url):
+    """Fetch the content of the given URL and return the response text."""
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  # Check if the request was successful
+        return response.text
+    except requests.RequestException as e:
+        print(f"Error fetching {url}: {e}")
+        return None
+
+def beautify_text(response_text):
+    """Parse the response text with BeautifulSoup and return the BeautifulSoup object."""
+    if response_text:
+        return BeautifulSoup(response_text, 'html.parser')
+    else:
+        return None
+
+def get_items_title(soup):
+    """Extract and return the titles of promotion items from the BeautifulSoup object."""
+    if soup:
+        html_tags = soup.find_all('p', class_='promotion-item__title')
+        return [tag.get_text(strip=True) for tag in html_tags]
+    else:
+        return []
+
+def get_items_price(soup):
+    """Extract and return the prices of promotion items from the BeautifulSoup object."""
+    if soup:
+        html_tags = soup.find_all('span', class_='promotion-item__installments')
+        return [tag.get_text(strip=True) for tag in html_tags]
+    else:
+        return []
+    
+def main(url):
+    """Main function to fetch and print promotion item titles and prices."""
+    response_text = get_response_text(url)
+    soup = beautify_text(response_text)
+    titles = get_items_title(soup)
+    prices = get_items_price(soup)
+    products = {title: price for title, price in zip(titles, prices)}
+    print(products)
+
+
+if __name__ == "__main__":
+    url = 'https://www.mercadolivre.com.br/ofertas'
+    main(url)
