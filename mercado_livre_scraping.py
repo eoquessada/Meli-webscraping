f42bc20beb3a76535b82f1b44e1be2682b125c8d
eoquessada <eoquessada@gmail.com>
2024-06-05 23:06:13 -0300

    scrap_all_pages func and remove get_items_price func and get_items_price_descriptions func

--- mercado_livre_scraping.py
+++ mercado_livre_scraping.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
+import pandas as pd
 
 def get_response_text(url):
     """Fetch the content of the given URL and return the response text."""
@@ -26,45 +27,21 @@ def get_items_title(soup):
     else:
         return []
 
-def get_items_price(soup):
-    """Extract and return the prices of promotion items from the BeautifulSoup object."""
-    if soup:
-        prices = []
-        html_tags_currency = soup.find_all('span', class_='andes-money-amount__currency-symbol')
-        html_tags_price = soup.find_all('span', class_='andes-money-amount__fraction')
-        for i in range(len(html_tags_currency)):
-            currency = html_tags_currency[i]
-            price = html_tags_price[i]
-            full_price = f"{currency} {price}"
-            prices.append(full_price)
-        return prices
-    else:
-        return []
+def scrap_all_pages(base_url, max_pages, column_name):
+    """Scrap all pages from offer products"""
+    products = []
+    for i in range(max_pages):
+        page_url = f"{base_url}{i+1}"
+        response_text = get_response_text(page_url)
+        soup = beautify_text(response_text)
+        titles = get_items_title(soup)
+        if titles:
+            products.extend(titles)
+        else:
+            break  # Stop if no titles are found on the page
+    return pd.DataFrame(products, columns=[column_name])
 
-def get_items_price_description(soup):
-    """Extract and return the prices descriptions of promotion items from the BeautifulSoup object."""
-    if soup:
-        prices_descriptions = []
-        html_tags = soup.find_all('span', class_='promotion-item__installments')
-        for tag in html_tags:
-            main_price = tag.contents[0].strip()
-            centavos = tag.find('sup').get_text(strip=True)
-            full_price_description = f"{main_price}.{centavos}"
-            prices_descriptions.append(full_price_description)
-        return prices_descriptions
-    else:
-        return []
-    
-def main(url):
-    """Main function to fetch and print promotion item titles and prices."""
-    response_text = get_response_text(url)
-    soup = beautify_text(response_text)
-    titles = get_items_title(soup)
-    prices = get_items_price_description(soup)
-    products = {title: price for title, price in zip(titles, prices)}
-    print(products)
-
-
-if __name__ == "__main__":
-    url = 'https://www.mercadolivre.com.br/ofertas'
-    main(url)
+url = 'https://www.mercadolivre.com.br/ofertas?container_id=MLB779362-1&page='
+max_pages = 20
+offers = scrap_all_pages(url, max_pages, 'offers')
+print(offers)
